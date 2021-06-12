d35bf8c839b5af59ca73b57c9cf0ec281e06d5bf
Vinicius Hedler <viniciushedler@gmail.com>
2021-06-11 23:30:09 -0300

    Created docstrings

--- coronashooter/background.py
+++ coronashooter/background.py
@@ -4,43 +4,55 @@ from math import ceil
 
 
 class Background:
-    """
-    Esta classe cria o fundo do jogo
+    """ Creates the background of the game
     """
 
     def __init__(self, image="space.png"):
+        """ Background constructor
+        :param image: the image to go on the background
+        :type image: string
         """
-        Desenha o fundo da tela
-        """
-        image = os.path.join('images', image)
-        image = pygame.image.load(image).convert()
 
+        # loads the image
+        image = os.path.join('images', image)
+        image = pygame.image.load(image).convert() # .convert() is for performance. Pygame bulls**t
+
+        # sets the initial variables
         self.imagesize = image.get_size()
         self.pos = [0, -1 * self.imagesize[1]]
         screen = pygame.display.get_surface()
         screen_size = screen.get_size()
 
+        # sets the width and height
         w = (ceil(float(screen_size[0]) / self.imagesize[0]) + 1) * \
             self.imagesize[0]
         h = (ceil(float(screen_size[1]) / self.imagesize[1]) + 1) * \
             self.imagesize[1]
-
         back = pygame.Surface((w, h))
 
+        # blit many copies of the background so it fills the whole back of the screen
         for i in range((back.get_size()[0] // self.imagesize[0])):
             for j in range((back.get_size()[1] // self.imagesize[1])):
                 back.blit(image, (i * self.imagesize[0], j * self.imagesize[1]))
 
         self.image = back
 
+
     def update(self, dt):
+        """ Moves the background
+        :param dt: unused
+        :type dt: float (?)
+        """
         self.pos[1] += 1
         if self.pos[1] > 0:
             self.pos[1] -= self.imagesize[1]
 
-    # update()
 
     def draw(self, screen):
+        """ Draw it's image on the given screen
+        :param screen: screen to be drawn on
+        :type screen: pygame.screen
+        """
         screen.blit(self.image, self.pos)
 
 
--- coronashooter/elements.py
+++ coronashooter/elements.py
@@ -3,31 +3,47 @@ import os
 
 
 class ElementSprite(pygame.sprite.Sprite):
-    """
-    Esta é a classe básica de todos os objetos do jogo.
+    """ The basic class of every element in the game.
+    Inherits from pygame.sprite.Sprite so pygame can do with it everything it does with sprites
     """
 
     def __init__(self, image, position, speed=None, new_size=None):
+        """ Element constructor
+        :param image: the location and name of the desired image for the sprite
+        :type image: string
+        :param position: the initial position of the element
+        :type position: list
+        :param speed: the initial speed of the element
+        :type speed: list
+        :param new_size: the desired size of the loaded image
+        :type new_size: list
+        """
         pygame.sprite.Sprite.__init__(self)
-        self.image = image
-        if isinstance(self.image, str):
-            self.image = os.path.join('images', self.image)
-            self.image = pygame.image.load(self.image)
+        # Try to load the image
+        if isinstance(image, str):
+            self.image = pygame.image.load(os.path.join('images', image))
+        else:
+            raise TypeError("image must be of type str")
+        # Checks if the image is to be resized and do so if it is
         if new_size:
             self.scale(new_size)
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect() # gets a pygame.Rect object out of the image
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
 
-        self.set_pos(position)
-        self.set_speed(speed or (0, 2))
+        self.set_pos(position) # sets the position of the sprite
+        self.set_speed(speed or (0, 2)) # sets the speed. If None, the speed is set to (0,2)
 
-    # __init__()
 
     def update(self, dt):
+        """ Updates the position of the element
+        :param dt: unused
+        :type dt: float (?)
+        """
         move_speed = (self.speed[0] * dt / 16,
-                      self.speed[1] * dt / 16)
+                      self.speed[1] * dt / 16) # note that dt=16, so dt/16 == 1
         self.rect = self.rect.move(move_speed)
+        # kills the element if it is out of the screen borders
         if (self.rect.left > self.area.right) or \
                 (self.rect.top > self.area.bottom) or \
                 (self.rect.right < 0):
@@ -35,33 +51,25 @@ class ElementSprite(pygame.sprite.Sprite):
         if (self.rect.bottom < - 40):
             self.kill()
 
-    # update()
 
     def get_speed(self):
         return self.speed
 
-    # get_speed()
-
     def set_speed(self, speed):
         self.speed = speed
 
-    # set_speed()
-
     def get_pos(self):
+        # gets the position of the pygame.Rect object
         return (self.rect.center[0],
                 self.rect.bottom)
 
-    # get_pos()
-
     def set_pos(self, pos):
+        # sets the position of the pygame.Rect object
         self.rect.center = (pos[0], pos[1])
-
-    # get_pos()
 
     def get_size(self):
         return self.image.get_size()
 
-    # get_size()
-
     def scale(self, new_size):
+        # resizes the sprite
         self.image = pygame.transform.scale(self.image, new_size)
