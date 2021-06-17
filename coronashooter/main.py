441394c4e9cee2699a3e56bdff8c7eb630f6523f
sjphelt <sylviojorge2010@hotmail.com>
2021-06-17 16:41:39 -0300

    Update main.py

--- coronashooter/main.py
+++ coronashooter/main.py
@@ -25,7 +25,7 @@ class Jogo:
 
         self.screen_size = self.screen.get_size()
         pygame.mouse.set_visible(0)
-        pygame.display.set_caption('Corona Shooter')
+        pygame.display.set_caption('Corona Shooter v4')
         self.run = True
 
     def atualiza_elementos(self, dt):
