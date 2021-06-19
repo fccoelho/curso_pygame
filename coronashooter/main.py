1b919aeb56e632b0fba8b99efd205332ec301b12
adamesalles <adamesalles111@gmail.com>
2021-06-19 17:04:35 -0300

    Melhorando movimentação e spawn de inimigos

--- coronashooter/main.py
+++ coronashooter/main.py
@@ -15,24 +15,27 @@ import random
 
 class Game:
     def __init__(self, size=(640, 640), fullscreen=False):
-        """ Create the object that will control the game
+        """ Creates the object that will control the game
 
         :param size: desired size of the screen
         :type size: tuple
-        :param fullscreen: wether the game will be played on fullscreen or not
+        :param fullscreen: whether the game will be played on fullscreen or not
         :type fullscreen: boolean
         """
-        self.elements = {}  # create the dict which will have all elements of the game
-        pygame.init()  # Initiate the pygame module
-        flags = DOUBLEBUF | FULLSCREEN if fullscreen else DOUBLEBUF  # set the display flags
-        # create the display with the set size and flags
+        self.elements = {}  # creates the dict which will have all elements of the game
+        self.enemies = []
+        self.spwcounter = 45
+        pygame.init()  # Initiates the pygame module
+        flags = DOUBLEBUF | FULLSCREEN if fullscreen else DOUBLEBUF  # sets the display flags
+        # creates the display with the set size and flags
         self.screen = pygame.display.set_mode(size, flags)
-        self.background = Background()  # create the background object
-        # set the game's window title
-        pygame.display.set_caption('Corona Shooter')
-        pygame.mouse.set_visible(0)  # make the mouse cursor invisible
+        self.background = Background()  # creates the background object
+        # sets the game's window title
+        pygame.display.set_caption('PC Virus Shooter')
+        pygame.mouse.set_visible(0)  # makes mouse cursor invisible
         self.run = True
-        self.loop()  # start running the game
+        self.level = 1
+        self.loop()  # starts running the game
 
     def update_elements(self, dt):
         """ Updates the background so it feels like moving
@@ -41,45 +44,60 @@ class Game:
         :type dt: float (?)
         """
         self.background.update(dt)
+        for enemy in self.enemies:
+            enemy[0].update(dt)
 
     def draw_elements(self):
-        """ Draw the elements onto the screen.
+        """ Draws the elements onto the screen.
         This is done by passing the screen as a parameter to the element that will draw itself on the screen.
         """
         self.background.draw(self.screen)  # draw the background
         for element in self.elements.values():
             element.draw(self.screen)
+        for enemy in self.enemies:
+            enemy[1].draw(self.screen)
 
     def handle_events(self, event, dt=16):
-        """ Handle each event in the event queue.
-        This is basically only used for the user to control the spaceship and to quit the game
+        """ Handles each event in the event queue.
+        This is basically only used for the user to control the spaceship and quit the game
         """
-        # handle window quit (the "x" on top right corner)
+        # handles window quit (the "x" on top right corner)
         if event.type == pygame.QUIT:
             self.run = False
-        # handle keyboard input
+        # handles keyboard input
         if event.type in (KEYDOWN,):
             key = event.key
-            if key == K_ESCAPE:  # handle keyboard quit
+            if key == K_ESCAPE:  # handles keyboard quit
                 self.run = False
+
+    def spawn(self):
+        pos_x = random.randint(0, 640)
+        if self.spwcounter > 60:
+            virus = Enemy([pos_x, 0])
+            self.enemies.append([virus, pygame.sprite.RenderPlain(virus)])
+            self.spwcounter = 0
+        else:
+            self.spwcounter += 1
 
     def loop(self):
         """ Main game loop
         """
-        clock = pygame.time.Clock()  # create the clock object
+        clock = pygame.time.Clock()  # creates the clock object
         dt = 16  # defines the effective speed of the game
         self.player = Player([200, 400], 5)
+        self.virus = Enemy([120, 50])
         self.elements['coronavirus'] = pygame.sprite.RenderPlain(
-            Virus([120, 50]))  # prepare the coronavirus sprite
+            self.virus)  # prepares the coronavirus sprite
         self.elements['player'] = pygame.sprite.RenderPlain(
-            self.player)  # prepare the spacechip sprite
+            self.player)  # prepares the spaceship sprite
         while self.run:
             clock.tick(1000 / dt)
             event = pygame.event.poll()
-            # Handle the user interaction
+            # Handles the user interaction
             self.player.update(event, dt)
             self.handle_events(event, dt)
-            # Handle the position and colision updates
+            self.spawn()
+            # Handles the position and colision updates
             self.update_elements(dt)
 
             # Draw the elements on their new positions. Right now only works for the background
@@ -109,7 +127,7 @@ class Spaceship(ElementSprite):
         """
         self.acceleration = [
             3, 3]  # sets the initial acceleration of the spaceship
-        image = "syringe.png" if not image else image  # sets the default image
+        image = "virus.png" if not image else image  # sets the default image
         # calls ElementSprite.__init__()
         self.direction = (0, 0)
         super().__init__(image, position, speed, new_size)
@@ -122,9 +140,9 @@ class Spaceship(ElementSprite):
         self.lives = lives
 
 
-class Virus(Spaceship):
-    def __init__(self, position, lives=0, speed=None, image=None, size=(100, 100)):
-        """ Virus constructor. Basically the same as Spaceship.__init__(), only difference is the default value for image
+class Enemy(Spaceship):
+    def __init__(self, position, lives=0, speed=.35, image=None, size=(75, 50)):
+        """  constructor. Basically the same as Spaceship.__init__(), only difference is the default value for image
         :param position: the initial position of the element
         :type position: list
         :param lives: how many times the element can get hit before dying
@@ -136,37 +154,46 @@ class Virus(Spaceship):
         :param new_size: the desired size of the sprite. See ElementSprite.scale()
         :type new_size: list
         """
-        image = "virus.png" if not image else image
+        image = "inimigo1G.png" if not image else image
         super().__init__(position, lives, speed, image, size)
 
 
 class Player(Spaceship):
-    def __init__(self, position, lives=10, image=None, new_size=(83, 248)):
+    def __init__(self, position, lives=10, speed=.5, image=None, new_size=(27, 36)):
         if not image:
-            image = "syringe.png"
-        super().__init__(position, lives, 1, image, new_size)
+            image = "nave1.png"
+        super().__init__(position, lives, speed, image, new_size)
         self.score = 0
+        self.size = new_size
 
     def update(self, event, dt):
         if event.type in (KEYDOWN,):
             key = event.key
-            if key == K_ESCAPE:  # handle keyboard quit
-                self.run = False
-            elif key in (K_UP, K_DOWN, K_RIGHT, K_LEFT):
+            if key in (K_UP, K_DOWN, K_RIGHT, K_LEFT):
                 if key == K_UP and self.rect.top >= 0:
                     self.direction = (0, -1)
                 elif key == K_DOWN:
                     self.direction = (0, 1)
                 elif key == K_RIGHT:
+                    self.set_image('nave2.png', self.size)
                     self.direction = (1, 0)
                 elif key == K_LEFT:
+                    self.set_image('nave3.png', self.size)
                     self.direction = (-1, 0)
         elif event.type in (KEYUP,):
             self.direction = (0, 0)
-        pos_x = self.rect.center[0] + self.rect.center[0] * \
-            self.direction[0]*self.speed/dt
-        pos_y = self.rect.center[1] + self.rect.center[1] * \
-            self.direction[1]*self.speed/dt
+            self.set_image('nave1.png', self.size)
+        pos_x = self.rect.center[0] + self.direction[0]*self.speed*dt
+        pos_y = self.rect.center[1] + self.direction[1]*self.speed*dt
+        if pos_x < 0:
+            pos_x = 640
+        elif pos_x > 640:
+            pos_x = 0
+        if pos_y < 0:
+            pos_y = 0
+        elif pos_y > 640:
+            pos_y = 640
+        print(self.rect.center)
         self.rect.center = (pos_x, pos_y)
 
     def get_pos(self):
@@ -179,5 +206,8 @@ class Player(Spaceship):
         self.score = score
 
 
+# como faz pra jogar mesmo/ foi de agora a pergunta?
+# foi, nao sei como faz.. #so ta rodando pro edu :(
+# :.3 oiඞNAOOOOOOOOOOOඞඞඞඞඞ  sus  _(:.3 J L)_ ඞ
 if __name__ == '__main__':
     G = Game()
