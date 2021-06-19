12d4358c8fb8c7445498e8dc5b00da6ff9da5f5a
adamesalles <adamesalles111@gmail.com>
2021-06-19 19:40:19 -0300

    Adicionando visual novo e colisões

--- coronashooter/main.py
+++ coronashooter/main.py
@@ -9,7 +9,7 @@ from pygame.locals import (DOUBLEBUF,
                            K_ESCAPE, K_UP, K_DOWN, K_RCTRL, K_LCTRL
                            )
 from background import Background
-from elements import ElementSprite
+from elements import *
 import random
 
 
@@ -24,12 +24,16 @@ class Game:
         """
         self.elements = {}  # creates the dict which will have all elements of the game
         self.enemies = []
+        self.shoots = []
         self.spwcounter = 45
+        self.colcounter = 0
+        self.color = 'G'
         pygame.init()  # Initiates the pygame module
         flags = DOUBLEBUF | FULLSCREEN if fullscreen else DOUBLEBUF  # sets the display flags
         # creates the display with the set size and flags
         self.screen = pygame.display.set_mode(size, flags)
-        self.background = Background()  # creates the background object
+        # creates the background object
+        self.background = Background('fundoG.png')
         # sets the game's window title
         pygame.display.set_caption('PC Virus Shooter')
         pygame.mouse.set_visible(0)  # makes mouse cursor invisible
@@ -45,7 +49,10 @@ class Game:
         """
         self.background.update(dt)
         for enemy in self.enemies:
+            # , self.player.rect.center[0] - Necessário para spyder
             enemy[0].update(dt)
+        for shoot in self.shoots:
+            shoot[0].update(dt)
 
     def draw_elements(self):
         """ Draws the elements onto the screen.
@@ -56,13 +63,15 @@ class Game:
             element.draw(self.screen)
         for enemy in self.enemies:
             enemy[1].draw(self.screen)
+        for shoot in self.shoots:
+            shoot[1].draw(self.screen)
 
     def handle_events(self, event, dt=16):
         """ Handles each event in the event queue.
         This is basically only used for the user to control the spaceship and quit the game
         """
         # handles window quit (the "x" on top right corner)
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or self.player.isdead:
             self.run = False
         # handles keyboard input
         if event.type in (KEYDOWN,):
@@ -73,21 +82,36 @@ class Game:
     def spawn(self):
         pos_x = random.randint(0, 640)
         if self.spwcounter > 60:
-            virus = Enemy([pos_x, 0])
-            self.enemies.append([virus, pygame.sprite.RenderPlain(virus)])
+            enemy = Shooter([pos_x, -25], color=self.color)
+            self.enemies.append([enemy, pygame.sprite.RenderPlain(enemy)])
             self.spwcounter = 0
         else:
             self.spwcounter += 1
+
+    def handle_collision(self):
+        for enemy in self.enemies:
+            plyr_collision = self.player.rect.colliderect(enemy[0].rect)
+            if plyr_collision and self.colcounter <= 0:
+                print('ui')
+                self.player.got_hit()
+                self.colcounter = 60
+            for shoot in self.shoots:
+                enemy_collision = enemy[0].rect.colliderect(shoot[0].rect)
+                if enemy_collision:
+                    enemy[0].got_hit()
+                    self.player.add_score()
+                    if enemy[0].get_lives() <= 0:
+                        self.enemies.remove(enemy)
+                    self.shoots.remove(shoot)
+        if self.colcounter > 0:
+            self.colcounter -= 1
 
     def loop(self):
         """ Main game loop
         """
         clock = pygame.time.Clock()  # creates the clock object
         dt = 16  # defines the effective speed of the game
-        self.player = Player([200, 400], 5)
-        self.virus = Enemy([120, 50])
-        self.elements['coronavirus'] = pygame.sprite.RenderPlain(
-            self.virus)  # prepares the coronavirus sprite
+        self.player = Player([320, 540], 5)
         self.elements['player'] = pygame.sprite.RenderPlain(
             self.player)  # prepares the spaceship sprite
         while self.run:
@@ -95,7 +119,9 @@ class Game:
             event = pygame.event.poll()
             # Handles the user interaction
             self.player.update(event, dt)
+            self.player.shoot(event, self.shoots)
             self.handle_events(event, dt)
+            self.handle_collision()
             self.spawn()
             # Handles the position and colision updates
             self.update_elements(dt)
@@ -129,7 +155,6 @@ class Spaceship(ElementSprite):
             3, 3]  # sets the initial acceleration of the spaceship
         image = "virus.png" if not image else image  # sets the default image
         # calls ElementSprite.__init__()
-        self.direction = (0, 0)
         super().__init__(image, position, speed, new_size)
         self.set_lives(lives)  # sets the lives of the spaceship
 
@@ -140,31 +165,15 @@ class Spaceship(ElementSprite):
         self.lives = lives
 
 
-class Enemy(Spaceship):
-    def __init__(self, position, lives=0, speed=.35, image=None, size=(75, 50)):
-        """  constructor. Basically the same as Spaceship.__init__(), only difference is the default value for image
-        :param position: the initial position of the element
-        :type position: list
-        :param lives: how many times the element can get hit before dying
-        :type lives: integer (?)
-        :param speed: the initial speed of the element on both axis
-        :type speed: list
-        :param image: the image of the element. The class has a default value that gets overwriten when this parameter is not None
-        :type image: string
-        :param new_size: the desired size of the sprite. See ElementSprite.scale()
-        :type new_size: list
-        """
-        image = "inimigo1G.png" if not image else image
-        super().__init__(position, lives, speed, image, size)
-
-
 class Player(Spaceship):
-    def __init__(self, position, lives=10, speed=.5, image=None, new_size=(27, 36)):
+    def __init__(self, position, lives=3, speed=.5, image=None, new_size=(27, 36)):
         if not image:
             image = "nave1.png"
         super().__init__(position, lives, speed, image, new_size)
+        self.direction = (0, 0)
         self.score = 0
         self.size = new_size
+        self.isdead = False
 
     def update(self, event, dt):
         if event.type in (KEYDOWN,):
@@ -193,10 +202,21 @@ class Player(Spaceship):
             pos_y = 0
         elif pos_y > 640:
             pos_y = 640
-        print(self.rect.center)
         self.rect.center = (pos_x, pos_y)
 
-    def get_pos(self):
+    def shoot(self, event, shoots):
+        if event.type in (KEYDOWN,):
+            key = event.key
+            if key == K_LCTRL:
+                laser = Laser((self.rect.center[0], self.rect.top))
+                shoots.append([laser, pygame.sprite.RenderPlain(laser)])
+
+    def got_hit(self):
+        self.lives -= 1
+        if self.lives <= 0:
+            self.isdead = True
+
+    def get_pos(self):  # perdao
         return (self.rect.center[0], self.rect.center[1])
 
     def get_score(self):
@@ -204,6 +224,17 @@ class Player(Spaceship):
 
     def set_score(self, score):
         self.score = score
+        print(self.score)
+
+    def add_score(self):
+        self.score += 1
+
+
+class Laser(ElementSprite):
+    def __init__(self, position, speed=.6, image=None):
+        if not image:
+            image = "tironave1.png"
+        super().__init__(image, position, speed, direction=(0, -1))
 
 
 # como faz pra jogar mesmo/ foi de agora a pergunta?
